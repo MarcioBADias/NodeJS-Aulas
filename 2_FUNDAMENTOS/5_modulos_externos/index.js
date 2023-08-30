51297d1fa396e845417fe7bda4fd92133597436f
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-08-30 16:53:06 -0300

    Revisando

--- 2_FUNDAMENTOS/5_modulos_externos/index.js
+++ 2_FUNDAMENTOS/5_modulos_externos/index.js
@@ -1,8 +1,10 @@
 const minimist = require('minimist')
 
 const args = minimist(process.argv.slice(2))
-// console.log(args)
+console.log(args)
 
 const name = args['name']
 const profissao = args['profissao']
 console.log(`Ola! Me chamo ${name} e sou ${profissao}.`)
+
+// No minimist antes de entrar com os argumento nós digitamos "--" para interagir com ARGV
