c37b0340e855b03d3bfa7876be33646287808853
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-08-30 18:18:16 -0300

    explorando console (basico)

---
+++ 2_FUNDAMENTOS/7_explorando_o_console/index.js
@@ -0,0 +1,15 @@
+// coisas novas:
+const x = 10
+
+// contador pelo console
+console.count(`o valor de x é ${x}, contagem`)
+console.count(`o valor de x é ${x}, contagem`)
+console.count(`o valor de x é ${x}, contagem`)
+console.count(`o valor de x é ${x}, contagem`)
+console.count(`o valor de x é ${x}, contagem`)
+
+// limpar o console
+
+setTimeout(()=>{
+    console.clear()
+},2000)
