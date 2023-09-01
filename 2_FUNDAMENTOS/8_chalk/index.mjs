b35b59a5e968cd106cc9d95722b3d395524f2d1b
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-01 09:09:57 -0300

    chalk atualizado

--- 2_FUNDAMENTOS/8_chalk/index.mjs
+++ 2_FUNDAMENTOS/8_chalk/index.mjs
@@ -6,5 +6,5 @@ const nota = 3
 if(nota >= 7){
     console.log(chalk.green(`Parabéns você foi aprovado! Sua nota foi ${nota}.`))
 }else{
-    console.log(chalk.red(`Que pena, você não foi aprovado, sua nota foi ${nota}.`))
+    console.log(chalk.bgRed(`Que pena, você não foi aprovado, sua nota foi ${nota}.`))
 }
