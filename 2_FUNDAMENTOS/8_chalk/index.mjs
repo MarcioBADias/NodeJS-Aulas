6ec468ebc597648e19e9c132f30c45e0c4841ed0
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-03 10:07:41 -0300

    atualiazndo mais sobre Chalk

--- 2_FUNDAMENTOS/8_chalk/index.mjs
+++ 2_FUNDAMENTOS/8_chalk/index.mjs
@@ -1,10 +1,10 @@
 import chalk from 'chalk'
 
-const nota = 3
+const nota = 8
 
 
 if(nota >= 7){
-    console.log(chalk.green(`Parabéns você foi aprovado! Sua nota foi ${nota}.`))
+    console.log(chalk.bgGreen(`Parabéns você foi aprovado! Sua nota foi ${nota}.`))
 }else{
     console.log(chalk.bgRed(`Que pena, você não foi aprovado, sua nota foi ${nota}.`))
 }
