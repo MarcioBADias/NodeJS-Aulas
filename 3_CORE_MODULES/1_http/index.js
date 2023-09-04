55d25a5983c64763ac2d55f6800305f4d6a44bb5
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-04 16:34:29 -0300

    http

--- 3_CORE_MODULES/1_http/index.js
+++ 3_CORE_MODULES/1_http/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const port = 3000
 
 const server = http.createServer((req, res) => {
-    res.write('Oi HTTP')
+    res.write('Oi HTTP ESTOU FUNFANDO DIREITINHO')
     res.end()
 })
 
