c7e80df0ff49dc33f52a67041b8acde9ce48fafb
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-04 16:44:56 -0300

    no

---
+++ 3_CORE_MODULES/2_retornando_html/index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('contenty-Type', 'text/html')
+    res.end(
+        `<h1>Seja bem vindo ao meu primeiro server HTML!</h1>`
+    )
+})
+
+server.listen(port, () => {
+    console.log(`O servidor está funcionando na porta ${port}`)
+})
