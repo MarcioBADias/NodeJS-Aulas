afb8a0f9d203cfadfad1d188ed2b092ff49b5de2
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-05 21:29:49 -0300

    atualização do projeto

--- 3_CORE_MODULES/2_retornando_html/index.js
+++ 3_CORE_MODULES/2_retornando_html/index.js
@@ -11,5 +11,5 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(port, () => {
-    console.log(`O servidor está funcionando na porta ${port}`)
+    console.log(`O servidor está funcionando na porta ${port} com força total!`)
 })
