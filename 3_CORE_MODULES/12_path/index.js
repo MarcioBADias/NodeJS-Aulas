dd750992b9c1c79e0872016600ff1bc8eff45c58
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-07 20:24:29 -0300

    path

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const customPath = '/relatorios/marcio/relatorio1.pdf'
+
+console.log(path.basename(customPath))
+console.log(path.dirname(customPath))
+console.log(path.extname(customPath))
