129cbc2218933927070b99cae21ff062021fdced
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-19 21:25:02 -0300

    carregando parametros pelo url

---
+++ 6_EXPRESS/6_param_url/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path = require('path')
+const basePath = path.join(__dirname, 'templates')
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+
+    console.log(`Estamos buscando pelo usuário ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
