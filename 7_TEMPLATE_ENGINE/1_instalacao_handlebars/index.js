5cd73e5eb8dcae2b21a7a9dc8323d8059abdde0e
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-22 16:50:36 -0300

    consertando o problema com o express-handlebars

--- 7_TEMPLATE_ENGINE/1_instalacao_handlebars/index.js
+++ 7_TEMPLATE_ENGINE/1_instalacao_handlebars/index.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
+const express = require('express');
+const { engine }  = require('express-handlebars');
 
-const app = express()
+const app = express();
 
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 
-app.get('/', (req,res) => {
-    res.render('home', { layout: false })
-})
+app.get('/', (req, res) => {
+    res.render('home', { layout: false });
+});
 
 app.listen(3000, () => {
-    console.log('server on na porta 3000')
-})
+    console.log('Servidor na porta 3000');
+});
