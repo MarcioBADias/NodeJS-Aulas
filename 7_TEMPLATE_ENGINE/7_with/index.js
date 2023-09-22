68a2cb672afa17bdb94d23154ab0fc106d34e763
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-09-22 18:58:54 -0300

    with

---
+++ 7_TEMPLATE_ENGINE/7_with/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const { engine }  = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+
+app.get('/post', (req, res) => {
+    const post = {
+        title: 'Aprendendo Node JS',
+        category: 'backend',
+        body: 'Nesse aprendizado voce vai poder...',
+        coments: 4
+    }
+
+    res.render('blogpost', {post})
+})
+
+app.get('/dashboard', (req, res) => {
+    const password = 'teste'
+    const itens = ['item 01', 'item 02', 'item 03']
+    res.render('dashboard', {password, itens})
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        name: "Marcio",
+        age: 35
+    }
+
+    const password = 'teste'
+
+    const auth = user.age > 17
+
+    res.render('home', { user, password, auth })
+})
+
+app.listen(3000, () => {
+    console.log('Servidor na porta 3000')
+})
