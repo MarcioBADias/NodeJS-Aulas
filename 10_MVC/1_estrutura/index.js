add489c182cf7277642624f7b1a5d94f4fd03943
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-10-14 17:52:17 -0300

    criando estrutura de mvc para iinicio de projeto

---
+++ 10_MVC/1_estrutura/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const conn = require('./db/conn')
+
+const app = express()
+
+app.engine('handlebars', engine())
+app.set('viwes engine', 'handlebars')
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.listen(3000)
